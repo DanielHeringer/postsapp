be7232f062d27e8d9392bc28cd56522bd7ea120c
Daniel Heringer <danielheringer2012@gmail.com>
2020-04-30 12:48:02 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 const port = process.env.port || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(port);
   app.useGlobalPipes(new ValidationPipe());
   Logger.log(`Listening ${process.env.APP_URL}:${port}`, 'Bootstrap')
